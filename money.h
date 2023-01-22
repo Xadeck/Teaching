25543d5d4e0ddb865f25081d65e6ae1f40d88bdb
Xavier François Décoret <{ID}+{username}@users.noreply.github.com>
2023-01-22 09:51:46 +0100

    Added classes to represent a bank account with transactions.

--- money.h
+++ money.h
@@ -1,6 +1,31 @@
 #pragma once
 
-class Money {
-  public:
-  private:
+#include <string>
+#include <vector>
+
+class Category {
+public:
+  std::string name;
+};
+
+class Transaction {
+public:
+  // Negative for an expense, positive for an income.
+  float amount;
+  Category category;
+};
+
+class Account {
+public:
+  float Balance() const {
+    float balance = 0.0f;
+
+    for (auto t : transactions) {
+      balance += t.amount;
+    }
+
+    return balance;
+  }
+
+  std::vector<Transaction> transactions;
 };
