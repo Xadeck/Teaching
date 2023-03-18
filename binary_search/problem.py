e1dffafdb3e4ebf504bf287b65a8f6da248a5a6e
Xavier Décoret <{ID}+{username}@users.noreply.github.com>
2023-03-18 16:59:16 +0100

    Fix some bugs and prepare for Max work

--- binary_search/problem.py
+++ binary_search/problem.py
@@ -4,6 +4,9 @@ import datetime
 import random
 import time
 
+# Uncomment the next line to always generate the same random list.
+#
+# random.seed(123)
 
 birthdays = []
 
@@ -13,7 +16,7 @@ for i in range(1000000):
     day = random.randint(1,28)
     birthdays.append((datetime.date(year, month, day), "P %d" % i))
 
-Nina = (datetime.date(1988, 1, 27), "Nina")
+Nina = (datetime.date(1988, 12, 27), "Nina")
 birthdays.append(Nina)
 
 moment_a = time.time()
@@ -26,7 +29,7 @@ moment_c = time.time()
 
 print("before ", birthdays[index-1])
 print("Nina is", birthdays[index])
-print("after  ", birthdays[index+1])
+print("after  ", birthdays[(index+1) % len(birthdays)])
 
 print("Time to sort", moment_b - moment_a)
 print("Time to find", moment_c - moment_b)
