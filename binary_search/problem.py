31e23dc488d31c4c2ccca0f1da5e5786d91cf78c
Xavier Décoret <{ID}+{username}@users.noreply.github.com>
2023-03-18 16:47:42 +0100

    Create a simple example for searching

---
+++ binary_search/problem.py
@@ -0,0 +1,34 @@
+#!/usr/local/bin/python3
+
+import datetime
+import random
+import time
+
+
+birthdays = []
+
+for i in range(1000000):
+    year = random.randint(1975, 2006)
+    month = random.randint(1,12)
+    day = random.randint(1,28)
+    birthdays.append((datetime.date(year, month, day), "P %d" % i))
+
+Nina = (datetime.date(1988, 1, 27), "Nina")
+birthdays.append(Nina)
+
+moment_a = time.time()
+birthdays = sorted(birthdays,
+                   key=lambda p:(p[0].month, p[0].day))
+moment_b = time.time()
+
+index = birthdays.index(Nina)
+moment_c = time.time()
+
+print("before ", birthdays[index-1])
+print("Nina is", birthdays[index])
+print("after  ", birthdays[index+1])
+
+print("Time to sort", moment_b - moment_a)
+print("Time to find", moment_c - moment_b)
+
+
